5c39edad612e9727ee37ee48443a98f6ea77f307
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-08-09 19:14:13 -0400

    Recreate Washington command example with SVG counters

--- Documents/Washingtons_Crossing/prepare_svg_counter_assets.sh
+++ Documents/Washingtons_Crossing/prepare_svg_counter_assets.sh
@@ -8,6 +8,23 @@ counter_dir="$script_dir/../../Counters/Washingtons_Crossing"
 output_dir="$counter_dir/svg-inkscape"
 
 mkdir -p "$output_dir"
-for counter in american-leader-cadwalader british-leader-rall durham-boats washington-leader-display; do
+for counter in \
+  american-detachment-4 \
+  american-leader-cadwalader \
+  american-leader-dickinson \
+  american-leader-fermoy \
+  american-leader-greene \
+  american-leader-hitchcock \
+  american-leader-lord-stirling \
+  american-leader-mcdougall \
+  american-leader-mercer \
+  american-leader-mifflin \
+  american-leader-sargent \
+  american-leader-sullivan \
+  american-leader-washington \
+  british-leader-rall \
+  durham-boats \
+  washington-leader-display
+do
   rsvg-convert -f pdf -o "$output_dir/${counter}_svg-raw.pdf" "$counter_dir/$counter.svg"
 done
